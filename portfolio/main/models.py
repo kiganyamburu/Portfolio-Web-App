f9905d351adf12f32e2414122f8736744d90331b
kiganyamburu <mburukiganya@gmail.com>
2024-05-26 17:54:42 +0300

    correcting indentation error

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Tag(models.model):
     name  = models.CharField(max_length=200, unique=True)
         
-    def__str__(self):
+    def __str__(self):
         return self.name
 
 class Project(models.Model):

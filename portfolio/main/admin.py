431e1bc9866500dcc4a2d0975175884894f8a889
kiganyamburu <mburukiganya@gmail.com>
2024-05-26 21:14:12 +0300

    working on the add

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import Tag, Project, ProjectImage
 
-# Register your models here.
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "link")
+    inlines = []
+    search_fields = ("title", "description")
+    list_filter = ("tags", )
